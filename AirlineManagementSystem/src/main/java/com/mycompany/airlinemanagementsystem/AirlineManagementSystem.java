5d47c9f163531c30351098101b942171ac41d678
VENKATA RAMANA KONDAPALLI <heyykvr@gmail.com>
2023-04-05 12:06:12 +0530

    Add files via upload

---
+++ AirlineManagementSystem/src/main/java/com/mycompany/airlinemanagementsystem/AirlineManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.airlinemanagementsystem;
+
+/**
+ *
+ * @author Realme
+ */
+public class AirlineManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
